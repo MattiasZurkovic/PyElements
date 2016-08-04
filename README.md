65af5e232413d3e51c5927d2db775581853dd650
Mattias Zurkovic <mattias@zurkovic.com>
2016-08-04 11:23:01 -0700

    seperate methods & add foremat_info function as 'master' parser of json

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ PyElements is a Python library/module that allows for access of various informat
 ## Usage
 As of now, PyElements ships with several usable methods. They are listed below:
 * `.get_by_name(element_name)` - This method retrives a given elements information by passing it name. The `element_name` is the parameter for the name of the desired element, for example "Hydrogen".
-* `get_by_atomic_number(atomic_number)` - Gets the information of the element by atomic number. The `atomic_number` parameter is the atomic number of the desired element.
+* `.get_by_atomic_number(atomic_number)` - Gets the information of the element by atomic number. The `atomic_number` parameter is the atomic number of the desired element.
--- elements_index.py
+++ elements_index.py
@@ -7,12 +7,24 @@ parsed_data = json.loads(response.read())
 
 element_names = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "Oxygen", "Fluorine", "Sodium", "Magnesium", "Aluminum", "Silicon", "Phosphorus", "Sulfur", "Chlorine", "Potassium", "Calcium", "Scandium", "Titanium", "Vanadium", "Chromium", "Manganese", "Iron", "Cobalt", "Nickel", "Copper", "Zinc", "Gallium", "Germanium", "Arsenic", "Selenium", "Bromine", "Krypton", "Rubidium", "Strontium", "Yttrium", "Zirconium", "Niobium", "Molybdenum", "Technetium", "Ruthenium", "Rhodium", "Palladium", "Silver", "Cadmium", "Indium", "Tin", "Antimony", "Tellurium", "Iodine", "Xenon", "Cesium", "Barium", "Lanthanum", "Cerium", "Praseodymium", "Neodymium", "Promethium", "Samarium", "Europium", "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium", "Thulium", "Ytterbium", "Lutetium", "Hafnium", "Tantalum", "Tungsten", "Rhenium", "Osmium", "Iridium", "Platinum", "Gold", "Mercury", "Thallium", "Lead", "Bismuth", "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium", "Thorium", "Protactinium", "Uranium", "Neptunium", "Plutonium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium", "Mendelevium", "Nobelium", "Lawrencium", "Rutherfordium", "Dubnium", "Seaborgium", "Bohrium", "Hassium", "Meitnerium", "Ununnilium", "Unununium", "Ununbium", "Ununtrium", "Ununquadium", "Ununpentium", "Ununhexium", "Ununseptium", "Ununoctium"]
 
+json_keys = ["atomic_number", "abbreviation", "atomic_weight"]
+
+def format_info(target):
+	elem_values = []
+        target_json = parsed_data[target]
+        for i in json_keys:
+                elem_values.append(target_json[i])
+	return elem_values
+	
+
 def get_by_name(elem_name):
-	return parsed_data[elem_name.upper()]
+	return format_info(elem_name.upper())
 
 def get_by_atomic_number(atomic_number):
 	element_ans = element_names[atomic_number - 1]
-	return parsed_data[element_ans.upper()]
-
+	return format_info(element_ans.upper()) 
+ 
 print get_by_name("Thorium")
 print get_by_atomic_number(4)
+
+print parsed_data['HYDROGEN']['atomic_number']
