2833f8ab92ec524d43969a83f1a9ca925cdbb01f
Mattias Zurkovic <mattias@zurkovic.com>
2016-08-04 21:02:52 -0700

    implement check for weirdly spelt elements. (I.E. Aluminium)

--- elements_index.py
+++ elements_index.py
@@ -8,7 +8,7 @@ element_json = "https://gist.githubusercontent.com/peterellisjones/758549cd18d46
 response = urllib2.urlopen(element_json)
 parsed_data = json.loads(response.read())
 
-element_names = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "Oxygen", "Fluorine", "Sodium", "Magnesium", "Aluminum", "Silicon", "Phosphorus", "Sulfur", "Chlorine", "Potassium", "Calcium", "Scandium", "Titanium", "Vanadium", "Chromium", "Manganese", "Iron", "Cobalt", "Nickel", "Copper", "Zinc", "Gallium", "Germanium", "Arsenic", "Selenium", "Bromine", "Krypton", "Rubidium", "Strontium", "Yttrium", "Zirconium", "Niobium", "Molybdenum", "Technetium", "Ruthenium", "Rhodium", "Palladium", "Silver", "Cadmium", "Indium", "Tin", "Antimony", "Tellurium", "Iodine", "Xenon", "Cesium", "Barium", "Lanthanum", "Cerium", "Praseodymium", "Neodymium", "Promethium", "Samarium", "Europium", "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium", "Thulium", "Ytterbium", "Lutetium", "Hafnium", "Tantalum", "Tungsten", "Rhenium", "Osmium", "Iridium", "Platinum", "Gold", "Mercury", "Thallium", "Lead", "Bismuth", "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium", "Thorium", "Protactinium", "Uranium", "Neptunium", "Plutonium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium", "Mendelevium", "Nobelium", "Lawrencium", "Rutherfordium", "Dubnium", "Seaborgium", "Bohrium", "Hassium", "Meitnerium", "Ununnilium", "Unununium", "Ununbium", "Ununtrium", "Ununquadium", "Ununpentium", "Ununhexium", "Ununseptium", "Ununoctium"]
+element_names = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "Oxygen", "Fluorine", "Neon", "Sodium", "Magnesium", "Aluminum", "Silicon", "Phosphorus", "Sulfur", "Chlorine", "Argon", "Potassium", "Calcium", "Scandium", "Titanium", "Vanadium", "Chromium", "Manganese", "Iron", "Cobalt", "Nickel", "Copper", "Zinc", "Gallium", "Germanium", "Arsenic", "Selenium", "Bromine", "Krypton", "Rubidium", "Strontium", "Yttrium", "Zirconium", "Niobium", "Molybdenum", "Technetium", "Ruthenium", "Rhodium", "Palladium", "Silver", "Cadmium", "Indium", "Tin", "Antimony", "Tellurium", "Iodine", "Xenon", "Cesium", "Barium", "Lanthanum", "Cerium", "Praseodymium", "Neodymium", "Promethium", "Samarium", "Europium", "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium", "Thulium", "Ytterbium", "Lutetium", "Hafnium", "Tantalum", "Tungsten", "Rhenium", "Osmium", "Iridium", "Platinum", "Gold", "Mercury", "Thallium", "Lead", "Bismuth", "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium", "Thorium", "Protactinium", "Uranium", "Neptunium", "Plutonium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium", "Mendelevium", "Nobelium", "Lawrencium", "Rutherfordium", "Dubnium", "Seaborgium", "Bohrium", "Hassium", "Meitnerium", "Ununnilium", "Unununium", "Ununbium", "Ununtrium", "Ununquadium", "Ununpentium", "Ununhexium", "Ununseptium", "Ununoctium"]
 
 json_keys = ["atomic_number", "abbreviation", "atomic_weight"]
 
@@ -25,16 +25,39 @@ def table_info(info):
 	print "| Atomic Number | " + str(info[0])
 	print "| Atomic Mass   | " + str(info[-1])	
 
+# This method checks the spelling type of the element
+def check_name(prov):
+	if prov == "Aluminum":
+		return "Aluminium"
+	elif prov == "Sulfur":
+		return "Sulphur"
+	elif prov == "Cesium":
+		return "Caesium"
+	else:
+		return True
+
 def get_by_name(elem_name):
-	return format_info(elem_name.upper())
+	elem_checked = check_name(elem_name)
+	if elem_checked != True:
+		return format_info(elem_checked.upper())
+	else: # Default to...
+		return format_info(elem_name.upper())
 
 def get_by_atomic_number(atomic_number):
 	element_ans = element_names[atomic_number - 1]
-	return format_info(element_ans.upper()) 
+	if check_name(element_ans) != True:
+		return format_info(check_name(element_ans).upper()) 
+	else:
+		return format_info(element_ans.upper())
+
+print get_by_name("Cesium")
 
 
+for i in range (1, 99):
+	print table_info(get_by_atomic_number(i))
  
 print get_by_name("Thorium")
 table_info(get_by_atomic_number(4))
+print get_by_atomic_number(1)
 
 
